870798f9442a2c3a634f6acff6578c0ebcac62ed
jyothi72 <jyothilax72@gmail.com>
2023-04-11 12:24:01 +0530

    restaurant app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from'react'
+import  Headered from './components/layout/Header'
+function App() {
+  return (
+    <React.Fragment>
+      <Headered/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import classes from "./header.module.css";
+import HeaderButton from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpg";
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h2>React Meal</h2>
+        <HeaderButton/>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="food" />
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
