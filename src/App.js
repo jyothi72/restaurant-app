94eed8bc09262d8f36bee147cd34a1b6b2c028d2
jyothi72 <jyothilax72@gmail.com>
2023-04-11 14:14:27 +0530

    added a pop up containing meals summary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from'react'
+import Meals from './components/Meals/meals';
 import  Headered from './components/layout/Header'
 function App() {
   return (
     <React.Fragment>
       <Headered/>
+      <main>
+         <Meals/>
+      </main>
     </React.Fragment>
   );
 }
---
+++ src/components/Meals/meals.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MealsSummary from "./mealssummary"
+
+import AvailableMeals from "./availablemeals"
+const Meals=()=>{
+ return(   <div>
+        <MealsSummary/>
+        <AvailableMeals/>
+    </div>
+ )
+}
+
+
+export default Meals
